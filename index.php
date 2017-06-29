c3dbf38ecc6551fc1167204fb3e92dd4a1ee1dfb
Alexander Mikhailov <chrono@smtp.ru>
2017-06-29 15:27:58 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<header>
+		<div><?php get_header(); ?></div>
+	</header>
+	<div> Site </div>
+	<footer>
+		<?php get_footer(); ?>
+	</footer>
+</body>
+</html>
